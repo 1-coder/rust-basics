f1dd7bc33740d0492067a1649b206aad8db1e139
1-coder <wasim.ipy@gmail.com>
2024-09-12 12:01:26 +0000

    handling user input

--- hello_rust/src/main.rs
+++ hello_rust/src/main.rs
@@ -1,3 +1,27 @@
+use std::io;
+
+fn str_to_int(str: &String) -> Result<i32, String> {
+    str.trim()
+        .parse::<i32>()
+        .map_err(|_| "Inavlid Input, Enter a valid number".to_string())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+
+    println!("Enter a number");
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    match str_to_int(&input) {
+        Ok(num) => match num % 2 == 0 {
+            true => println!("{} is Even", num),
+            false => println!("{} is Odd", num),
+        },
+        Err(e) => {
+            println!("{e}")
+        }
+    }
 }
